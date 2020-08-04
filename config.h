7fb21537cec6cdbe08abbb9709ee524b3188e7fd
Kho-Dialga <ivandashenyou@gmail.com>
2020-08-04 09:32:49 -0600

    Added font2 patch

--- config.h
+++ config.h
@@ -5,7 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=12";
+/* Spare fonts */
+static char *font2[] = {
+	"Noto Color Emoji:pixelsize=12:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
