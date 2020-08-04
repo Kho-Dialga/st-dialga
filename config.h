345853f149d9404cedb8e94bb91f400ef9ae77bd
Kho-Dialga <ivandashenyou@gmail.com>
2020-08-04 13:49:18 -0600

    Applied font2 patch

--- config.h
+++ config.h
@@ -5,7 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Nerd Font Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Nerd Font:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"Noto Color Emoji:pixelsize=12:antialias=true:autohint=true", // Emojis
+	"Hack Nerd Font Mono:pixelsize=14:antialias=true:autohint=true", // Powerline
+};
+
 static int borderpx = 2;
 
 /*
