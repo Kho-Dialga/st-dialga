2d55d5c791c088d2a81a7cbaff9a75e85a84b369
Kho-Dialga <ivandashenyou@gmail.com>
2020-08-04 09:26:18 -0600

    Applied my colorscheme

--- config.h
+++ config.h
@@ -99,31 +99,31 @@ float alpha = 0.9;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#1d2021",
+	"#b02626",
+	"#40a626",
+	"#f2e635",
+	"#314ad0",
+	"#b30ad0",
+	"#32d0fc",
+	"#acadb1",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#676f78",
+	"#b55454",
+	"#78a670",
+	"#faf380",
+	"#707fd0",
+	"#c583d0",
+	"#8adaf1",
+	"#e0e3e7",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-	"black",
+	"#191919",
 };
 
 
