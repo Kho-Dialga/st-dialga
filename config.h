a999f4348d88e2e17ecc64674e1a4f08e98a7e03
Kho-Dialga <ivandashenyou@gmail.com>
2020-08-07 19:53:02 -0600

    Re-added all the fonts and shortcuts

--- config.h
+++ config.h
@@ -8,7 +8,15 @@
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Symbola:pixelsize=16:antialias=true:autohint=true",
+	"Noto Color Emoji:pixelsize=12:antialias=true:autohint=true", // Emoji
+	"Noto Sans CJK JP:pixelsize=16:style=Bold:antialias=true:autohint=true", // Japanese
+	"Noto Sans Runic:pixelsize=16:antialias=true:autohint=true", // Runes
+	"Noto Sans Georgian:pixelsize=16:antialias=true:autohint=true", // Georgian
+	"Noto Sans Thai:pixelsize=16:style=Bold:antialias=true:autohint=true", // Thai
+	"Noto Serif Ethiopic:style=Bold:pixelsize=16:antialias=true:autohint=true", // Ethiopiuc
+	"Noto Sans Hebrew:style=Bold:pixelsize=16:antialias=true:autohint=true", // Hebrew
+	"Noto Sans Math:pixelsize=16:antialias=true:autohint=true", // Math Symbols
+	"Symbola:pixelsize=16:antialias=true:autohint=true", // Symbols
 };
 
 static int borderpx = 2;
@@ -240,7 +248,7 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD (Mod1Mask|ShiftMask)
 
 static char *openurlcmd[] = { "/bin/sh", "-c", "st-urlhandler", "externalpipe", NULL };
 
@@ -250,23 +258,29 @@ static char *copyurlcmd[] = { "/bin/sh", "-c",
 
 static char *copyoutput[] = { "/bin/sh", "-c", "st-copyout", "externalpipe", NULL };
 
-
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ TERMMOD,              XK_K,       	zoom,           {.f = +1} },
+	{ TERMMOD,              XK_J,        	zoom,           {.f = -1} },
+	{ TERMMOD,              XK_U,       	zoom,           {.f = +1} },
+	{ TERMMOD,              XK_D,        	zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ MODKEY,               XK_c,           clipcopy,       {.i =  0} },
+	{ MODKEY,               XK_v,           clippaste,      {.i =  0} },
+	{ MODKEY,               XK_y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ MODKEY,            	XK_k,     	kscrollup,      {.i =  1} },
+	{ MODKEY,            	XK_j,   	kscrolldown,    {.i =  1} },
+	{ MODKEY,            	XK_u,     	kscrollup,      {.i = -1} },
+	{ MODKEY,            	XK_d,   	kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
+	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
+	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
 };
 
 /*
